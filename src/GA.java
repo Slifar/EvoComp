a9a03e89babf84cbf0b35a9a8e2f2149980b3e3a
Slifar <flichin@hotmail.com>
2014-04-17 14:10:11 -0500

    Minor update

--- src/GA.java
+++ src/GA.java
@@ -9,6 +9,7 @@ public class GA {
 	ArrayList<Chromasome> children = new ArrayList<Chromasome>(populationSize);
 	ArrayList<Chromasome> rouletteTable = new ArrayList<Chromasome>(100);
 	Random rand = new Random();
+	Chromasome currentBest;
 	/**
 	 * Function to generate the initial population
 	 */
@@ -26,12 +27,29 @@ public class GA {
 		//begin GA loop
 		do{
 			parentRoulette();//Generate the parent pool for this generation
+			//Enter recombination loop
 			for(int i = 0; i < populationSize/2; i++){
 				int select1 = rand.nextInt(parents.size());
-				Chromasome parent1 = parents.remove(select1);
-				Chromasome parent2 = parents.remove(rand.nextInt(parents.size()));
+				Chromasome parent1 = parents.remove(select1);//Randomly select one parent
+				Chromasome parent2 = parents.remove(rand.nextInt(parents.size()));//Randomly select the other parent
+				children.add(genChild(parent1, parent2));//Generate one of the children
+				children.add(genChild(parent2, parent1));//Generate the other child
 			}
+			population = children;
+			population.set(0, currentBest);//We dump the first child for our current best, to provide elitism
+			
 		}while(!terminate);
+	}
+
+	/**
+	 * Method to recombine two chromasomes into a single child, 
+	 * using a single-point crossover
+	 * @param parent1 - The primary parent, whos first half will be used
+	 * @param parent2 - The secondary parent, whos second half will be used
+	 */
+	private Chromasome genChild(Chromasome parent1, Chromasome parent2) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/**
